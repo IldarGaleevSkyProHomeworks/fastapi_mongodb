e351d0d97e9e94f2cb63e15e549d8d73135c4773
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-04-08 23:36:21 +0700

    add lifespan

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 from routes.user import user
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Life span")
+    yield
+    print("Complete")
+    
+app = FastAPI(lifespan=lifespan)
 app.include_router(user)
 
 if __name__ == "__main__":
